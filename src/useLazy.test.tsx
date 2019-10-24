25e6ba90896aaaae2ca8a9054c5464e4454e9cd4
jero <aneurysmjs@outlook.com>
2019-10-24 17:50:46 +0300

    test(uselazy): upgrade test
    test when resolving promise when second parameter is given

--- src/useLazy.test.tsx
+++ src/useLazy.test.tsx
@@ -7,8 +7,13 @@ type DynamicModule = () => Promise<typeof import('./Example')>;
 const getModule: DynamicModule = () => import('./Example');
 
 describe('LazyComponent', () => {
-  it('should render "null" at first', () => {
-    const { result } = renderHook(() => useLazy(getModule));
+  it('should render "null" at first and then resolve promise', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useLazy(getModule, true));
     expect(result.current).toEqual(null);
+
+    await waitForNextUpdate();
+
+    expect(result.current).not.toBe(undefined);
+    expect(typeof result.current).toBe('function');
   });
 });
